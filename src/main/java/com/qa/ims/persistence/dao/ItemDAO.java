6870cbb3f514bcd664615f96ceeaeb67e4b3ccce
KesterJJ <kesterjjones@gmail.com>
2022-07-04 17:35:37 +0100

    SCFP-1 #done

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,45 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ItemDAO implements Dao {
+
+	@Override
+	public List readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object create(Object t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object update(Object t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
