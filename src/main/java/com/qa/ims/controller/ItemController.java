8784bf20caa6b12d03e6521dee8f43f4b98a3591
KesterJJ <kesterjjones@gmail.com>
2022-07-04 17:30:33 +0100

    SCFP-20 #done

---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,31 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+public class ItemController implements CrudController {
+
+	@Override
+	public List readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
