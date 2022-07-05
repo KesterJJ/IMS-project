b3dce053599b39236fd477957cfccdff1c99b41a
KesterJJ <kesterjjones@gmail.com>
2022-07-05 10:13:00 +0100

    SCFP-3 #done SCFP-6 #done

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE ims;
 
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
@@ -13,13 +14,22 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `item_name` VARCHAR(40) NOT NULL,
-    `price` DECIMAL(40) NOT NULL,
+    `price` DECIMAL(20, 2) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `customer_id` VARCHAR(40) NOT NULL,
+    `customer_id` INT(11) NOT NULL,
     FOREIGN KEY (`customer_id`) REFERENCES `ims`.`customers`(`id`),
     PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+`id` INT(11) NOT NULL auto_increment,
+`order_id` INT(11) NOT NULL,
+`item_id` INT(11) NOT NULL,
+FOREIGN KEY (`order_id`) REFERENCES `ims`.`orders`(`id`),
+FOREIGN KEY (`item_id`) REFERENCES `ims`.`items`(`id`),
+PRIMARY KEY (`id`)
 );
