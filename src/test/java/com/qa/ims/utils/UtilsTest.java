3459f2059ea1b4915e2cd6b78f35a2fdfe2f37b4
KesterJJ <kesterjjones@gmail.com>
2022-07-08 11:54:31 +0100

    added utils constructor test

--- src/test/java/com/qa/ims/utils/UtilsTest.java
+++ src/test/java/com/qa/ims/utils/UtilsTest.java
@@ -6,13 +6,23 @@ import static org.junit.Assert.assertTrue;
 import java.util.Scanner;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.qa.ims.persistence.domain.Customer;
 
 public class UtilsTest {
 	Utils utils = new Utils();;
-	
+
 	@Test
 	public void testUtils1() {
 		
+		assertTrue(utils instanceof Utils);
+	}
+	
+	@Test
+	public void testUtils2() {
+		Scanner scanner = new Scanner(System.in);
+		utils = new Utils(scanner);
 		assertTrue(utils instanceof Utils);
 	}
 	/*
@@ -21,13 +31,21 @@ public class UtilsTest {
 		Scanner scanner = new Scanner(System.in);
 		utils = new Utils(scanner);
 		assertTrue(utils instanceof Utils);
-	}
-	
+	}*/
+	/*
 	@Test
+
 	public void testGetLong() {
+
+		Mockito.when(utils.getString()).thenReturn("1", "1");
+
+		
 		assertEquals((Long) 1L, (Long) utils.getLong());
-	}
+
+		Mockito.verify(utils, Mockito.times(2)).getString();
+	}*/
 	
+	/*
 	@Test
 	public void testGetString() {
 		utils = new Utils();
@@ -39,4 +57,6 @@ public class UtilsTest {
 		utils = new Utils();
 		assertEquals((Double) 1d, utils.getDouble());
 	}*/
+	
+
 }
